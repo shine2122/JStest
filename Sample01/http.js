703fc85a3a8d337855fc1bd11af79759b6817ed6
Dongseop Kim <shine2122@gmail.com>
2018-11-22 16:52:14 +0900

    혼자 연습

--- Sample01/http.js
+++ Sample01/http.js
@@ -7,3 +7,16 @@ var port = 3000;//0~66635까지의 port가 있음
 server.listen(port, function() {
     console.log("Start server");
 });
+
+server.on('connection', function(socket)  {
+    var addr = socket.address();
+    console.log("누군가 접속함");
+});
+
+server.on('request', function(req, res) {
+    console.log("누군가 요청함");
+
+    res.writeHead(200, {"Content-type" : "text/html; charset=utf-8"});
+    res.write("반갑습니다.");
+    res.end();
+});
---
+++ Sample02/001.js
@@ -0,0 +1,19 @@
+
+let name = "DongSeop";
+let len = name.length;
+console.log(len);
+
+let newName = name.replace('Dong', 'Kim');
+console.log(newName);
+
+let char = newName.charAt(2);
+console.log(char);
+
+console.log(newName.toUpperCase());
+console.log(newName.toLowerCase());
+
+var x = 0;
+x += 5;
+console.log(x);
+console.log('hello' + 'world');
+console.log('1'+ 2 + 3 + 4 + 5);
